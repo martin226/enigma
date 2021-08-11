52b355a0737512dad1c33b55970798e8a74a534c
Martin Sit <46298951+martin-sit@users.noreply.github.com>
2021-08-10 23:48:57 -0400

    project setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,23 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true,
-  },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/airbnb',
-    '@vue/typescript/recommended',
-  ],
-  parserOptions: {
-    ecmaVersion: 2020,
-  },
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-  },
+    root: true,
+
+    env: {
+        node: true,
+    },
+
+    extends: [
+        'plugin:vue/recommended',
+        '@vue/airbnb',
+        '@vue/typescript/recommended',
+        'plugin:prettier/recommended',
+    ],
+
+    parserOptions: {
+        ecmaVersion: 2020,
+    },
+
+    rules: {
+        'no-console': 'off',
+        'no-debugger': 'off',
+    },
 };
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import Vue from 'vue';
 import App from './App.vue';
 import './registerServiceWorker';
+import './assets/css/index.css';
 
 Vue.config.productionTip = false;
 
 new Vue({
-  render: (h) => h(App),
+    render: (h) => h(App),
 }).$mount('#app');
