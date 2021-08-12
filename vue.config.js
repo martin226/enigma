e9015a9a51fc04103a685a9ce44304ad66e31e4e
Martin Sit <46298951+martin-sit@users.noreply.github.com>
2021-08-12 15:57:42 -0400

    fixed publicpath typo

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production' ? '/engima/' : '/',
+    publicPath: process.env.NODE_ENV === 'production' ? '/enigma/' : '/',
     pwa: {
         name: 'Enigma',
         themeColor: '#047857',
